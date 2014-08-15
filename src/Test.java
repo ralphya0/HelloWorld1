5a4bc5ff3e05d9cbb3751623f77add7fec3dcd87
ralphya0 <ralphya0@foxmail.com>
2014-08-15 21:07:45 +0800

    Test.java is updated just now...
    Signed-off-by: ralphya0 <ralphya0@foxmail.com>

--- src/Test.java
+++ src/Test.java
@@ -2,9 +2,9 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello,World!");
 		int vs = 0;
-		System.out.println(vs);
+		System.out.println("new variable is " + vs);
+		System.out.println(vs ++);
 	}
 
 }
